407f62240f096041ef5ecfd11a64de26b6a3ac8a
Maeli <maeli.palharini@hotmail.com>
2026-03-09 21:53:02 -0300

    feat: criar classe principal para aplicação AuraPay

---
+++ src/main/java/com/aurapay/AuraPayApplication.java
@@ -0,0 +1,11 @@
+package com.aurapay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuraPayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuraPayApplication.class, args);
+    }
+}
